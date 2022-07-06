08a4b6c447b8a2d4de7a3ed4b68c0819ed5f3779
Your Name <you@example.com>
2022-07-06 21:19:12 +0200

    end

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -15,6 +15,8 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: Padding(
+
+          /////
           padding: const EdgeInsets.all(48.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
